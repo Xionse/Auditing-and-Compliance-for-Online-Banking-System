af69c1e088e76bb041b506e96cb2bde0e9d8133d
Sexure <sexurexion@gmail.com>
2023-01-09 08:13:18 +0000

    Add the transactioncontent

---
+++ src/main/java/com/mycompany/onlinebanking/models/Transaction.java
@@ -0,0 +1,65 @@
+
+
+
+// This Transaction class represents a transaction in the online banking system. It has fields for the transaction's ID, account ID, type, amount, and creation date. The class is annotated with @Entity to indicate that it is a JPA entity, and the id field is annotated with @Id and @GeneratedValue to indicate that it is the primary key and should be generated automatically.
+
+package com.mycompany.onlinebanking.models;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private long accountId;
+    private String type;
+    private BigDecimal amount;
+    private Date createdAt;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
