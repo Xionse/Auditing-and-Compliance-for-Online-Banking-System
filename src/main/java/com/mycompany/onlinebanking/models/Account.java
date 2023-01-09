d66442f893762a3f513f9af735c63fda1ec509ea
Sexure <sexurexion@gmail.com>
2023-01-09 08:11:49 +0000

    Add the accountdet

---
+++ src/main/java/com/mycompany/onlinebanking/models/Account.java
@@ -0,0 +1,64 @@
+
+// This Account class represents an account in the online banking system. It has fields for the account's ID, user ID, type, balance, and creation date. The class is annotated with @Entity to indicate that it is a JPA entity, and the id field is annotated with @Id and @GeneratedValue to indicate that it is the primary key and should be generated automatically.
+
+
+package com.mycompany.onlinebanking.models;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private long userId;
+    private String type;
+    private BigDecimal balance;
+    private Date createdAt;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
